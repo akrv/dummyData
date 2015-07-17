1369c6e430023c35fcdf3b1306cdbd90e13290f7
akrv <aswinkarthik.rv@gmail.com>
2015-07-17 09:47:15 +0200

    staging commit
    works with Hazelcast 3.5 on raspberry pi

---
+++ src/dummydata/DummyData.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dummydata;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.IdGenerator;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author akrv
+ */
+public class DummyData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String propHostname = GetHostname();
+//        System.out.println(propHostname);
+        String[] dataSet;
+        String timeStamp, nodeMacAddress, snifferMacAddress, str;
+        //hazelcast parameters
+        Config cfg = new Config();
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+        IMap<String, String> mapSensors = instance.getMap("mapSensors");
+        IMap<String, String> snifferMap = instance.getMap("snifferMap");
+        
+        // to make mac address common for all nodes.
+        Map<Integer, String> listNodeMacAddress = instance.getMap("listNodeMacAddress");
+        Map<Integer, String> listSnifferMacAddress = instance.getMap("listSnifferMacAddress");
+        if ("master".equals(args[0])){
+        listNodeMacAddress.putAll(GenerateNodeMacAddress(listNodeMacAddress));
+        listSnifferMacAddress.putAll(GenerateNodeMacAddress(listSnifferMacAddress));
+        }
+        IdGenerator idGen = instance.getIdGenerator( "newId" );
+//        Map<String, String> macAddr = instance.getMap("macAddr");
+//        Map<String, String> gatewayNames = instance.getMap("gatewayNames");
+        
+        while (true){
+        dataSet = GenerateData();
+        nodeMacAddress = listNodeMacAddress.get(rand.nextInt(100));
+        dataSet[5] = propHostname;
+        dataSet[6] = nodeMacAddress;
+        str = Arrays.toString(dataSet);
+        timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+        mapSensors.put(timeStamp, str);
+        
+        dataSet = GenerateData();
+        snifferMacAddress = listSnifferMacAddress.get(rand.nextInt(100));
+        dataSet[5] = propHostname;
+        dataSet[6] = nodeMacAddress;
+        dataSet[7] = snifferMacAddress;
+        str = Arrays.toString(dataSet);
+        timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new java.util.Date());
+        snifferMap.put(timeStamp, str);
+        try {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException ie) {
+            System.out.println(ie);
+        }
+        
+        } 
+    }
+
+    private static String[] GenerateData() {
+        Random rand = new Random();
+        String[] value = new String[8];
+        for (int i =0;i<5;i++){
+        int a = rand.nextInt(50) + 1;
+        value[i] = a+"";
+        }
+        return value;
+    }
+    private static String GetHostname() {
+        // try environment properties.    
+        String host = System.getenv("COMPUTERNAME");
+        if (host != null)
+            return host;
+        host = System.getenv("HOSTNAME");
+        if (host != null)
+            return host;
+        // undetermined.
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(DummyData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return host;
+    }    
+
+
+    private static Map GenerateNodeMacAddress(Map macAddrList){
+        for (int i=0;i<100;i++){
+            String macAddr = randomMACAddress();
+            macAddrList.put(i,macAddr);  
+        }
+        return macAddrList;
+    }
+    
+    private static String randomMACAddress(){
+        Random rand = new Random();
+        byte[] macAddr = new byte[6];
+        rand.nextBytes(macAddr);
+        macAddr[0] = (byte)(macAddr[0] & (byte)254);  //zeroing last 2 bytes to make it unicast and locally adminstrated
+        StringBuilder sb = new StringBuilder(18);
+        for(byte b : macAddr){
+            if(sb.length() > 0)
+                sb.append(":");
+
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
